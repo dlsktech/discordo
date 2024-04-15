f7f57aae4b511dda4b4c80ae93a755f3364a15d0
Daniel Szewczuk <git@dlsk.tech>
2024-04-15 12:32:19 +0200

    Autoremove old versions of Discordo in install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,21 @@ if [ "$(id -u)" != "0" ]; then
     echo "This script requires root acces."
     exit 1
 fi
-clear
+
+file="/usr/bin/discordo"
+
+if [ -f "$file" ]; then
+    echo "Detected old version of Discordo. Removing..."
+    rm -rf $file
+fi
+
+file="/usr/discordo"
+
+if [ -f "$file" ]; then
+    echo "Detected old version of Discordo. Removing..."
+    rm -rf $file
+fi
+
 cd /usr/bin
 sudo curl https://raw.githubusercontent.com/danielszewczuk/Discordo/main/discordo.sh --output discordo
 sudo chmod +x discordo
